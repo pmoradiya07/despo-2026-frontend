6a8845669908653b594394e7ff5ef53f3fcd4c6d
pmoradiya07 <panthmoradiya7@gmail.com>
2025-12-28 21:41:34 +0400

    Added profile card and contact details

--- lib/profilepage.dart
+++ lib/profilepage.dart
@@ -42,29 +42,47 @@ class ProfileScreen extends StatelessWidget {
               ),
             ),
           ),
-          Column(
-            children: [
-              SizedBox(height: 50.h),
-              Row(
-                children: [
-                  SizedBox(width: 20.w),
-                  Expanded(child: PixelDashLine()),
-                  SizedBox(width: 10.w),
-                  Text(
-                    "Profile",
+          Center(
+            child: Column(
+              children: [
+                SizedBox(height: 50.h),
+                Row(
+                  children: [
+                    SizedBox(width: 20.w),
+                    Expanded(child: PixelDashLine(color: Colors.white70)),
+                    SizedBox(width: 10.w),
+                    Text(
+                      "Profile",
+                      style: GoogleFonts.jersey10(
+                        fontSize: 25.sp,
+                        color: Color.fromRGBO(255, 190, 38, 1),
+                      ),
+                    ),
+                    SizedBox(width: 10.w),
+                    Expanded(child: PixelDashLine(color: Colors.white70)),
+                    SizedBox(width: 20.w),
+                  ],
+                ),
+                SizedBox(height: 20.h),
+                ProfileCardWidget(),
+                SizedBox(height: 350.h),
+                GestureDetector(
+                  onTap: () {},
+                  child: Image.asset("assets/images/logout.png"),
+                ),
+                TextButton(
+                  onPressed: () => showCallPopup(context),
+                  child: Text(
+                    "Contact Us",
                     style: GoogleFonts.jersey10(
-                      fontSize: 25.sp,
-                      color: Color.fromRGBO(255, 190, 38, 1),
+                      color: Colors.white,
+                      fontSize: 20.sp,
                     ),
                   ),
-                  SizedBox(width: 10.w),
-                  Expanded(child: PixelDashLine()),
-                  SizedBox(width: 20.w),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
-          Image.asset("assets/images/profilecard.png")
         ],
       ),
     );
@@ -72,8 +90,8 @@ class ProfileScreen extends StatelessWidget {
 }
 
 class PixelDashLine extends StatelessWidget {
-  const PixelDashLine({super.key});
-
+  const PixelDashLine({super.key, required this.color});
+  final Color color;
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
@@ -89,11 +107,167 @@ class PixelDashLine extends StatelessWidget {
             (index) => Container(
               width: dotSize,
               height: dotSize,
-              color: Colors.white70,
+              color: color,
             ),
           ),
         );
       },
+    );
+  }
+}
+
+void showCallPopup(BuildContext context) {
+  showModalBottomSheet(
+    context: context,
+    backgroundColor: Colors.black87,
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+    ),
+    builder: (context) {
+      return Padding(
+        padding: EdgeInsets.symmetric(vertical: 20.h, horizontal: 16.w),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              leading: const Icon(Icons.call, color: Colors.green),
+              title: Text(
+                'Abhas Chaudhary',
+                style: GoogleFonts.jersey10(
+                  color: Colors.white,
+                  fontSize: 20.sp,
+                ),
+              ),
+              subtitle: Text(
+                'Fest Head',
+                style: TextStyle(color: Colors.white70),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.call, color: Colors.green),
+              title: Text(
+                'Arnav Rinawa',
+                style: GoogleFonts.jersey10(
+                  color: Colors.white,
+                  fontSize: 20.sp,
+                ),
+              ),
+              subtitle: Text(
+                'Fest Head',
+                style: TextStyle(color: Colors.white70),
+              ),
+              onTap: () {
+                // Navigator.pop(context);
+                // makeCall('+919123456789');
+              },
+            ),
+          ],
+        ),
+      );
+    },
+  );
+}
+
+class ProfileCardWidget extends StatelessWidget {
+  const ProfileCardWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Image.asset(
+          "assets/images/profilecard.png",
+          width: 320.w,
+          fit: BoxFit.contain,
+        ),
+
+        Positioned(
+          top: 10.h,
+          left: 110.w,
+          child: Row(
+            children: [
+              Text("Name : ", style: GoogleFonts.jersey10()),
+              Column(
+                
+                children: [
+                  Text("Panth Moradia", style: GoogleFonts.jersey10()),
+                  SizedBox(width: 150.w, child: PixelDashLine(color: Colors.black,)),
+                ],
+              ),
+              SizedBox(width: 20.h),
+            ],
+          ),
+        ),
+        Positioned(
+          top: 35.h,
+          left: 110.w,
+          child: Row(
+            children: [
+              Text("College : ", style: GoogleFonts.jersey10()),
+              Column(
+                children: [
+                  Text("LNMIIT", style: GoogleFonts.jersey10()),
+                  SizedBox(width: 140.w, child: PixelDashLine(color: Colors.black,)),
+                ],
+              ),
+              SizedBox(width: 20.h),
+            ],
+          ),
+        ),
+        Positioned(
+          top: 60.h,
+          left: 110.w,
+          child: Row(
+            children: [
+              Text("Accomodation : ", style: GoogleFonts.jersey10()),
+              Column(
+                
+                children: [
+                  Text("YES", style: GoogleFonts.jersey10()),
+                  SizedBox(width: 100.w, child: PixelDashLine(color: Colors.black,)),
+                ],
+              ),
+              SizedBox(width: 20.h),
+            ],
+          ),
+        ),
+        Positioned(
+          top: 85.h,
+          left: 110.w,
+          child: Row(
+            children: [
+              Text("Mess : ", style: GoogleFonts.jersey10()),
+              Column(
+                
+                children: [
+                  Text("NO", style: GoogleFonts.jersey10()),
+                  SizedBox(width: 150.w, child: PixelDashLine(color: Colors.black,)),
+                ],
+              ),
+              SizedBox(width: 20.h),
+            ],
+          ),
+        ),
+        Positioned(
+          top: 110.h,
+          left: 110.w,
+          child: Row(
+            children: [
+              Text("Pronite : ", style: GoogleFonts.jersey10()),
+              Column(
+                
+                children: [
+                  Text("YES", style: GoogleFonts.jersey10()),
+                  SizedBox(width: 140.w, child: PixelDashLine(color: Colors.black,)),
+                ],
+              ),
+              SizedBox(width: 20.h),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
