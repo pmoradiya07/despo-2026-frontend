b91fbe8c828ee042be6fdf956c26f025db8b111a
pmoradiya07 <panthmoradiya7@gmail.com>
2026-01-29 05:00:31 +0400

    fix flutter web deploy on vercel

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -58,7 +58,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "637e6e10ce78747f9a54de24a95fbeb9
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "a8ed2b3a29bd13bcd0dc832f0b76db36",
+"flutter_bootstrap.js": "88a97501281cc58c8ecd126d6bb6601d",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
